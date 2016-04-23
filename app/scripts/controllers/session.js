a5a1c01095e844a683b0c028463d6fbc6b979268
Pablo Arce Cascante <josepa_arce@hotmail.com>
2016-04-22 18:34:42 -0600

    sessions module implemented

--- app/scripts/controllers/session.js
+++ app/scripts/controllers/session.js
@@ -17,11 +17,8 @@ angular.module('swaptricksApp')
 
   // User resgistration
   $scope.create = function(user){
-
-    //Compare passwords... TO DO!
-    console.log(user);
     var data = {"username": user.username,
-    "password": user.password1,
+    "password": user.password,
     "firstname": user.firstname};
     //make the Call
     $http.post('http://api.swapingzone.com:3000/users', data).then(function successCallback(responce){
@@ -29,19 +26,45 @@ angular.module('swaptricksApp')
       console.log(responce);
       $location.path('/');
     }, function errorCallback(responce){
-      alert("Someting went wrong! :(");
+      WrongFeedback(responce);
       console.log(responce);
     });
   }
 
   // User login
   $scope.login = function(user){
-
+    var data = {"username": user.username, "password": user.password};
+    //make the Call
+    $http.post('http://api.swapingzone.com:3000/login', data).then(function successCallback(responce){
+      console.log(responce);
+      localStorage.token = responce.data.token;
+      $location.url('/about');
+    }, function errorCallback(responce){
+      WrongFeedback(responce);
+      console.log(responce);
+    });
   }
 
   // User logout
   $scope.logout = function(){
+    $http.post('http://api.swapingzone.com:3000/logout', {"token":localStorage.token})
+    .then(function successCallback(responce){
+      console.log(responce);
+      localStorage.token = null;
+      alert(responce.data.message);
+      $location.path('/');
+    }, function errorCallback(responce){
+      WrongFeedback(responce);
+      console.log(responce);
+    });
+  }
 
+  // Execute the bad responce from the errorCallback functions
+  function WrongFeedback(responce) {
+    if(responce.status == -1 || responce.status == 500)
+    alert("Problems... someting went wrong! try again later :(");
+    else
+    alert(responce.data.error);
   }
 
 });
